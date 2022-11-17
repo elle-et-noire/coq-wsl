d7db7a736a231d1f2e621b0e6a179b51e196c4a0
Lomega <atibon3@gmail.com>
2022-11-17 20:43:28 +0900

    sorted sort

--- sort/simplest01.v
+++ sort/simplest01.v
@@ -24,26 +24,6 @@ Proof.
   case H : (i < a); rewrite snd_destr_comm /=; apply f_equal, slide_hold_length.
 Qed.
 
-Program Fixpoint sort' l1 l2 {measure (length l2)} : list nat * list nat:=
-  match l2 with
-  | [] => (l1, [])
-  | h :: t => match slide h l1 with
-      (i, l1') => match slide i t with
-        (i', l2') => sort' (l1' ++ [i']) l2'
-      end
-    end
-  end.
-Obligation 1.
-rewrite /=. assert (l2' = snd (slide i t)). by rewrite -Heq_anonymous.
-by rewrite H -slide_hold_length.
-Qed.
-
-Definition sort (l:list nat) := fst (sort' [] l).
-
-Compute sort [3;1;4;1;5;9;2;6;5;3;5]. 
-Compute slide 5 [1;4;7;9].  
-Goal sort [3;1;4;1;5;9;2;6;5;3;5] = [1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9].
-Proof. by compute. Qed.
 
 Inductive sorted : list nat -> Prop :=
 | sorted_nil : sorted []
@@ -169,20 +149,6 @@ Proof.
   apply H3. apply S2.
 Qed. 
 
-(*
-Lemma sorted_trans_app_snoc : forall l1 l2 b, l2 <> [] ->
-  sorted (l1 ++ l2) -> sorted (l2 ++ [b]) -> sorted (l1 ++ l2 ++ [b]).
-Proof.
-  induction l2. done. move=> /= b _ S1 S2.
-  induction l1. done. induction l1.
-  move=> l1 l2 b S1 S2. 
-
-Lemma sorted_trans : forall l1 l2 l3,
-  sorted (l1 ++ l2) -> sorted (l2 ++ l3) -> sorted (l1 ++ l2 ++ l3).
-Proof.
-  induction l1. done. induction l1; move: IHl1=> /= IHl1 l2 l3 S1 S2.
-
-  case l2. rewrite /=.  *)
 
 Lemma slide_sorted : forall a l, sorted l -> sorted (snd (slide a l)).
 Proof. 
@@ -203,7 +169,6 @@ Lemma slide_geq_sorted : forall i l j, sorted l ->
 Proof.
   move => i l j. rewrite indep_prod. move: l i j. induction l.
   move=> i j S Hle; apply sorted_1. move: a.
-  
   move=> a i j S /=. 
   rewrite 2! indep_prod. 
   destruct l; case H: (i < a) => /= Hle.
@@ -243,61 +208,77 @@ Proof.
       { rewrite /=. case H2': (i < n); rewrite indep_prod /=. rewrite H2' in H2.
         inversion H2. done. }
       rewrite app_comm_cons H7. apply IHl. apply H5. rewrite -H6. apply Hle.
-    Qed.
+Qed.
 
-    case e.
-   apply sorted_cons. apply (leq_trans(n:=a)). apply ltnW, H.
-    
-    assert (sorted (snd (slide a l) ++ [j])).
-    { apply IHl. apply (cons_sorted _ _ S). apply Hle. }
-    destruct l. rewrite /=. apply sorted_cons. inversion Hle.
-    apply (leq_trans(n:=a)). apply ltnW, H. apply H2.
-    apply sorted_1. rewrite /= 2!indep_prod. case H2: (a < n).
-    rewrite /=. apply sorted_cons. apply ltnW, H.
-    apply 
-    Check leq_all_cons_sorted_snd.
+Lemma nill :{l | sorted l}.
+apply (exist _ nil sorted_nil). Qed.
+Check nill.
+Check (exist _ nil sorted_nil).
 
-  induction l.
-  move=> a i j S /=. case H: (i < a) => /= Hle. 
-   - apply sorted_cons. apply (leq_trans(n:=a)). apply ltnW, H. apply Hle.
-   - apply sorted_1.
-   - apply sorted_cons. move: H. rewrite ltnNge. move/eqP/eqP => H.
-    apply (leq_trans(n:=i)). apply H. apply Hle.
-   - apply sorted_1.
-   - move=> a0 i j S /=. rewrite 6!indep_prod. case H: (i < a0).
-     - case H2: (a0 < a) => /= Hle.
-       - apply sorted_cons. apply ltnW, H. pose (cons_sorted _ _ S).
-        pose ()
-  
-  move=> a i j S /=. rewrite 2! indep_prod. case H: (i < a) => /= Hle.
+Definition slidefb l1 h t :=
+  match slide h l1 with (i, l1') =>
+    match slide i t with (i', l2') => (l1' ++ [i'], l2')
+    end
+  end.
 
+Lemma sorted_sort'_fst : forall l1 h t, sorted l1 ->
+  match slide h l1 with (i, l1') => match slide i t with
+    (i', l2') => sorted (l1' ++ [i']) end end.
+Proof.
+  move=> l1 h t S. rewrite 2!indep_prod /=.
+  move: (slide_geq_sorted h l1 (fst (slide (fst (slide h l1)) t)) S).
+  rewrite indep_prod. apply. apply slide_leq.
+Qed.
 
-  induction l.
-   - move=> a i j /=. case H: (i < a) => /= Hle; apply sorted_cons.
-    apply (leq_trans(n:=a)). apply ltnW, H. apply Hle. apply sorted_1.
-    move: H. rewrite ltnNge. move/eqP/eqP => H. apply (leq_trans(n:=i)).
-    apply H. apply Hle. apply sorted_1.
-   - move=> a0 i j /=. rewrite 6!indep_prod. 
-  case H: (i < a0) => /=. case H2: (a0 < a) => /= Hle.
-  apply sorted_cons. apply ltnW, H. apply 
-  rewrite 2!indep_prod. induction l. case H: (i < a) => /= Hle. induction l.
-   - 
+Lemma sorted_sort'_fst' : forall l1 h t, sorted l1 -> sorted (fst (slidefb l1 h t)).
+Proof.
+  unfold slidefb. 
+  move=> l1 h t /=. assert (sorted l1 -> sorted 
+  (let (i, l1') := slide h l1 in
+   let (i', l2') := slide i t in l1' ++ [i'])).
+  move:(sorted_sort'_fst l1 h t). 
+   rewrite 4!indep_prod /=. apply.   
+  move:H. rewrite 4!indep_prod /=. apply.
+Qed.
 
+(* Definition sort'_noyau (l1s: {l | sorted l}) h t: {l | sorted l} :=
+  match l1s with exist l1 Sl1 => 
+    exist _ (fst (slidefb l1 h t)) (sorted_sort'_fst' _ h t Sl1)
+  end. *)
 
-  fix slide_geq_sorted 2.
-  move=> i [|h t] j. move=> H; apply sorted_1.
-  rewrite /= 3!indep_prod. case H: (i < h) => /= Hle.
-  induction t. rewrite /=. rewrite /= in Hle. apply sorted_cons.
-  apply (leq_trans(n := h)). apply ltnW, H. apply Hle.
-  apply sorted_1.
-  rewrite /= 2!indep_prod. case H2: (h < a) => /=. apply sorted_cons.
-  apply ltnW, H. apply IHt.
-  apply 
+Program Definition sort'_noyau (l1s: {l | sorted l}) h t : {l | sorted l} * list nat :=
+  (* match l1s with exist l1 Sl1 => slidefb l1 h t end. *)
+  slidefb l1s h t.
+Obligation 1.
+apply sorted_sort'_fst', H. Qed.
+
+Program Fixpoint sort' (l1s: {l | sorted l}) l2 {measure (length l2)} : {l | sorted l} * list nat :=
+  match l2 with
+  | [] => (l1s, [])
+  | h :: t => match sort'_noyau l1s h t with
+    (l1s', l2') => sort' l1s' l2' end
+  end.
+Obligation 1.
+move:sort' => /= sort'. unfold slidefb. 
+by rewrite 2!indep_prod /= -slide_hold_length.
+Qed.
+
+Definition sort (l:list nat) := (proj1_sig (fst (sort' (exist _ nil sorted_nil) l))).
+
+Compute sort [3;1;4;1;5;9;2;6;5;3;5]. 
+Compute slide 5 [1;4;7;9].  
+Goal sort [3;1;4;1;5;9;2;6;5;3;5] = [1; 1; 2; 3; 3; 4; 5; 5; 5; 6; 9].
+Proof. by compute. Qed.
 
 Lemma sort'_sorted : forall l1 l2, sorted l1 -> 
   sorted (fst (sort' l1 l2)).
 Proof.
-  fix sort'_sorted 2.
+  move=> l1 l2. move:l1. induction l2.
+  move=> l1 S. apply S.
+  move=> l1 S. unfold sort'.  unfold sort'_func. rewrite /=. indep_prod.  
+fix sort'_sorted 2.
+
+  move=> 
   intros l1. destruct l2. intros S.  simpl. apply S.
   intros S. 
   unfold sort', sort'_func. apply slide_sorted. simpl. snd. simpl. 
